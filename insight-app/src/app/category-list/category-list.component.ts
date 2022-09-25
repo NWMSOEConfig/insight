b9b83ac6a664a48a0bc7e3640b68e17621f0686f
Hunter Turcin <turcinh@msoe.edu>
2022-09-25 12:23:45 -0500

    Generate category list from data

--- insight-app/src/app/category-list/category-list.component.ts
+++ insight-app/src/app/category-list/category-list.component.ts
@@ -1,15 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 
+const tempData = [
+  {
+    name: 'Root',
+    children: [
+      {
+        type: 'category',
+        id: 1,
+      },
+      {
+        type: 'category',
+        id: 2,
+      },
+      {
+        type: 'category',
+        id: 3,
+      },
+    ]
+  },
+  {
+    name: 'Category A',
+    children: [
+      {
+        type: 'category',
+        id: 4,
+      },
+    ],
+  },
+  {
+    name: 'Category B',
+    children: [
+      {
+        type: 'category',
+        id: 5,
+      },
+    ],
+  },
+  {
+    name: 'Category C',
+    children: [
+      {
+        type: 'category',
+        id: 6,
+      },
+    ],
+  },
+  {
+    name: 'Subcategory 1',
+    children: [
+      {
+        type: 'setting',
+        id: 0,
+      },
+    ],
+  },
+  {
+    name: 'Subcategory 2',
+    children: [
+      {
+        type: 'setting',
+        id: 1,
+      },
+    ],
+  },
+  {
+    name: 'Subcategory 3',
+    children: [
+      {
+        type: 'setting',
+        id: 2,
+      },
+    ],
+  },
+]
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
+  categoryId = 0;
+  category = tempData[this.categoryId];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getCategory(id: number) {
+    return tempData[id];
+  }
 }
