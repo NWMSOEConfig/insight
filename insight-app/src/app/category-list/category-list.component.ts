d27a20c84808d3e10b581f4225308021251bfb0a
Hunter Turcin <turcinh@msoe.edu>
2022-09-25 21:13:10 -0500

    Abstract away category data access

--- insight-app/src/app/category-list/category-list.component.ts
+++ insight-app/src/app/category-list/category-list.component.ts
@@ -1,78 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-const tempData = [
-  {
-    name: 'Root',
-    children: [
-      {
-        type: 'category',
-        id: 1,
-      },
-      {
-        type: 'category',
-        id: 2,
-      },
-      {
-        type: 'category',
-        id: 3,
-      },
-    ]
-  },
-  {
-    name: 'Category A',
-    children: [
-      {
-        type: 'category',
-        id: 4,
-      },
-    ],
-  },
-  {
-    name: 'Category B',
-    children: [
-      {
-        type: 'category',
-        id: 5,
-      },
-    ],
-  },
-  {
-    name: 'Category C',
-    children: [
-      {
-        type: 'category',
-        id: 6,
-      },
-    ],
-  },
-  {
-    name: 'Subcategory 1',
-    children: [
-      {
-        type: 'setting',
-        id: 0,
-      },
-    ],
-  },
-  {
-    name: 'Subcategory 2',
-    children: [
-      {
-        type: 'setting',
-        id: 1,
-      },
-    ],
-  },
-  {
-    name: 'Subcategory 3',
-    children: [
-      {
-        type: 'setting',
-        id: 2,
-      },
-    ],
-  },
-]
+import { getCategory } from '../data-service';
 
 @Component({
   selector: 'app-category-list',
@@ -81,7 +9,7 @@ const tempData = [
 })
 export class CategoryListComponent implements OnInit {
   categoryId = 0;
-  category = tempData[this.categoryId];
+  category = getCategory(this.categoryId);
   breadcrumbs = [0];
 
   constructor() { }
@@ -89,19 +17,19 @@ export class CategoryListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getCategory(id: number) {
-    return tempData[id];
-  }
-
   click(child: {type: string, id: number}) {
     if (child.type === 'category') {
       this.categoryId = child.id;
-      this.category = tempData[this.categoryId];
+      this.category = getCategory(this.categoryId);
       this.breadcrumbs.push(child.id);
     }
   }
 
   getPath(): string {
-    return this.breadcrumbs.map(id => this.getCategory(id).name).join(' > ');
+    return this.breadcrumbs.map(id => getCategory(id).name).join(' > ');
+  }
+
+  getName(id: number): string {
+    return getCategory(id).name;
   }
 }
