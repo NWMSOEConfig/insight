cfad37bf5166d7c91df3ec546882eb5eb580bc5d
Kaif <sindhim@msoe.edu>
2022-10-03 20:01:06 -0500

    create ToDoItem

--- InsightApi/Program.cs
+++ InsightApi/Program.cs
@@ -1,19 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
