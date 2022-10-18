023eeded183a1507660491fcf8e7f1bb8d38bb3b
vermaam-36 <vermaa@msoe.edu>
2022-10-17 22:52:13 -0500

    Trying to figure out how this works

---
+++ Example Code/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+HttpClient client = new HttpClient();
+
+ HttpResponseMessage response = await client.GetAsync(
+                "https://pauat.newworldnow.com/api/applicationsettings");
+
+ response.EnsureSuccessStatusCode();
