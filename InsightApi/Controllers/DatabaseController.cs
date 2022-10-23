3868262b106cab8551c74d0b3ace5a57b5ec9a1a
Vaubel <vaubelg@msoe.edu>
2022-10-23 17:10:17 -0500

    added most api files for database endpoint

---
+++ InsightApi/Controllers/DatabaseController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+[ApiController]
+[Route("[api/Database]")]
+public class DatabaseController : ControllerBase {
+    
+}
+
+
+
