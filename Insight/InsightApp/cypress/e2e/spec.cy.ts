fd35d6076e95526ddb1ba87b710041252e2b2d2f
Kaif <sindhim@msoe.edu>
2022-10-31 18:29:33 -0500

    Revert "add cypress and write example test"
    This reverts commit 2fd362683ec00697937540eb6af549abcb036062.

--- Insight/InsightApp/cypress/e2e/spec.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('Example Read & Click Test', () => {
-  it('Visits the configuration page', () => {
-    cy.visit('/')
-    cy.contains('Category A')
-    cy.get('button').contains('Category A').click()
-    cy.contains('Subcategory 1')
-  })
-})
