e0f5598c0827d041902fedcd9212cf07755fffc4
Kaif <58781955+sindhim@users.noreply.github.com>
2023-01-20 18:05:34 -0600

    add paginator and mock commit card for history-page

--- Insight/InsightApp/src/app/history-page/history-page.component.ts
+++ Insight/InsightApp/src/app/history-page/history-page.component.ts
@@ -7,6 +7,39 @@ import { ApiService } from '../api-service/api.service';
   styleUrls: ['./history-page.component.css'],
 })
 export class HistoryPageComponent implements OnInit {
+  mockCommits: any = [
+    {
+      id: 'A92CE2L',
+      timestamp: '2023-01-09T01:56:47.909Z',
+      message: 'Modified all these settings because they were wrong before',
+      user: 'Kaif',
+      batch: [
+        {
+          settingName: '',
+          oldValue: '',
+          newValue: '',
+        },
+      ],
+    },
+    {
+      id: 'B93ASD3',
+      timestamp: '2023-01-13T09:12:03.909Z',
+      message: 'Creating settings for a new client in California',
+      user: 'Guy',
+      batch: [
+        {
+          settingName: '',
+          oldValue: '',
+          newValue: '',
+        },
+      ],
+    },
+  ];
+
+  getTimestamp(dateTime: any): Date {
+    return new Date(dateTime);
+  }
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {}
