71c675a6a41c7016b73ce643d93708a717469d1c
Kaif <58781955+sindhim@users.noreply.github.com>
2023-01-22 10:07:07 -0600

    move app-routing to its own folder

--- Insight/InsightApp/src/app/app.module.ts
+++ Insight/InsightApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
