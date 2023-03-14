e2d0e71452b8927e726658b271fa4615206daf6a
Kaif <58781955+sindhim@users.noreply.github.com>
2023-03-14 18:39:04 -0500

    rename tenant-selector to app-tenant-selector for consistency

--- Insight/InsightApp/src/app/tenant-selector/tenant-selector.component.ts
+++ Insight/InsightApp/src/app/tenant-selector/tenant-selector.component.ts
@@ -4,7 +4,7 @@ import { ApiService } from '../api-service/api.service';
 import { getTenant, setTenant, Tenant } from '../tenant-singleton';
 
 @Component({
-  selector: 'tenant-selector',
+  selector: 'app-tenant-selector',
   templateUrl: './tenant-selector.component.html',
   styleUrls: ['./tenant-selector.component.css'],
 })
