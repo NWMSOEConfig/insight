fc5dbcc8fd91f78586f66555d89f0a03b4eee0dc
Kaif <mkaifsindhi@gmail.com>
2023-04-14 12:41:43 -0500

    reload the publish page more appropriately

--- Insight/InsightApp/src/app/publish-page/publish-form/publish-page.form.ts
+++ Insight/InsightApp/src/app/publish-page/publish-form/publish-page.form.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ApiService } from 'src/app/api-service/api.service';
 
@@ -10,6 +10,7 @@ export type FormInterface = { field1: string; field2: string };
   styleUrls: ['./publish-page.form.css'],
 })
 export class PublishPageFormComponent implements OnInit {
+  @Input() queueRender!: any;
   showProgressBar = false;
   disableProgressBar = false;
   referencedId!: string;
@@ -27,26 +28,25 @@ export class PublishPageFormComponent implements OnInit {
 
   onPublishClicked() {
     this.showProgressBar = true;
+    this.disableProgressBar = true;
 
     if (!this.referencedId) {
       this.openSnackbar('Reference ID must be a number');
       this.showProgressBar = false;
-      this.disableProgressBar = true;
     } else if (parseInt(this.referencedId) <= 0) {
       this.openSnackbar('Reference ID must be a number greater than 0');
       this.showProgressBar = false;
-      this.disableProgressBar = true;
     } else if (!this.commitDescription) {
       this.openSnackbar('Description is a required field');
       this.showProgressBar = false;
-      this.disableProgressBar = true;
     } else {
       this.apiService
         .postPublish(this.commitDescription, parseInt(this.referencedId))
         .subscribe((data) => {
-          this.showProgressBar = false;
           this.openSnackbar('Changes have been successfully published');
-          window.location.reload();
+          this.queueRender.queue = {};
+          this.disableProgressBar = false;
+          this.showProgressBar = false;
         });
     }
   }
